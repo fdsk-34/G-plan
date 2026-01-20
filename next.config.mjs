35c3fc1b1f921500c1841d4b48e59defe1240bec
fdsk-34 <djfjfj2@naver.com>
2026-01-20 16:05:43 +0900

    리다이렉트 배포수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,11 +6,11 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
+  trailingSlash: true,
   // ⚠️ experimental이나 allowedDevOrigins는 로컬 개발용이므로 
   // 배포 시 문제가 된다면 아래와 같이 기본값으로 비워두는 것이 좋습니다.
   experimental: {
-    allowedDevOrigins: ["192.168.254.5", "localhost:3000"],
+    
   },
   
   // 빌드 시점에 정적 파일을 확실히 생성하도록 강제 (Optional)
